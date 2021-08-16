071145579a98d80ce7e25eca3ec583dff3a2b26e
hajira0037 <hajira@med-examexpert.com>
2021-08-16 20:21:20 +0500

    finally task done

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,54 +1,50 @@
-import React, { useEffect, useContext, useState } from "react";
-import { GlobalContext } from "../../context/GlobalState";
-import { getApiUrI } from "../../config";
-import firebase from "../../firebase";
-import axios from "axios";
-
-
-
+import React, { useEffect, useContext, useState } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
+import firebase from '../../firebase'
+import axios from 'axios'
 export default (props: any) => {
-  let { history } = props;
-  const { state, getDataAction, dispatch }: any = useContext(GlobalContext);
-  const [data, setData] = useState([]);
+  let { history } = props
+  const { state, getDataAction, dispatch }: any = useContext(GlobalContext)
+  const [data, setData] = useState([])
   useEffect(() => {
-    axios.get(getApiUrI).then((response) => {
-      setData(response.data);
-    });
-  }, []);
+    axios.get('/api').then(response => {
+      setData(response.data)
+    })
+  }, [])
 
   return (
-    <div className="">
-      <div className="py-5">
-        <div className="mt-8">
+    <div className=''>
+      <div className='py-5'>
+        <div className='mt-8'>
           <button
-            className="my-4 bg-green-400 hover:bg-green-500 w-full py-2 text-white"
+            className='my-4 bg-green-400 hover:bg-green-500 w-full py-2 text-white'
             onClick={() => {
               firebase
                 .auth()
                 .signOut()
                 .then(() => {
-                  state.loader = true;
-                  history.push("/login");
-                });
+                  state.loader = true
+                  history.push('/login')
+                })
             }}
           >
             Logout
           </button>
           <button
-            className="my-4 bg-green-400 hover:bg-green-500 w-full py-2 text-white"
+            className='my-4 bg-green-400 hover:bg-green-500 w-full py-2 text-white'
             onClick={() => getDataAction(dispatch)}
           >
             getData
           </button>
           {data.map((sin: any, index: number) => (
-            <div key={index} className="flex justify-between m-4">
-              <p key={index} className="text-base mb-2 flex  font-bold w-1/4">
+            <div key={index} className='flex justify-between m-4'>
+              <p key={index} className='text-base mb-2 flex  font-bold w-1/4'>
                 {sin.id} {sin.title}
               </p>
               <button
-                className=" bg-green-400 hover:bg-green-500 w-1/3 py-2 text-white w-1/4"
+                className=' bg-green-400 hover:bg-green-500 w-1/3 py-2 text-white w-1/4'
                 onClick={() => {
-                  props.history.push(`/${sin.id}`);
+                  props.history.push(`/${sin.id}`)
                 }}
               >
                 View Details
@@ -58,5 +54,5 @@ export default (props: any) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
