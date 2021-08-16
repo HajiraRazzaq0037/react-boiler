b6c475857935b6a00a0fef2e4ede688431604807
hajira0037 <hajira@med-examexpert.com>
2021-08-16 19:28:39 +0500

    parseInt added

--- src/pages/viewDetails/detail.tsx
+++ src/pages/viewDetails/detail.tsx
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { getApiUrI } from "../../config";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import { getApiUrI } from '../../config'
+import axios from 'axios'
 const Details = (props: any) => {
-  console.log();
   const [data, setData] = useState({
-    userId: "",
-    title: "",
-    body: "",
-  });
+    id: '',
+    title: '',
+    body: ''
+  })
   useEffect(() => {
-    axios.get(getApiUrI).then((response) => {
+    axios.get(getApiUrI).then((response: any) => {
       setData(
-        response.data.find((sin: any) => {
-          return sin;
-        })
-      );
-    });
-  }, [props.match.params.id]);
+        response.data.find(
+          (sin: any) => sin.id === parseInt(props.match.params.id)
+        )
+      )
+    })
+  }, [props.match.params.id])
   return (
-    <div className="">
-      <div className="py-5">user Detail</div>
-      <p>userId: {data.userId}</p>
+    <div className=''>
+      <div className='py-5'>user Detail</div>
+      <p>userId: {data.id}</p>
       <p>title: {data.title}</p>
       <p>body: {data.body}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Details;
+export default Details
